c11137081477cf331e75022a65d8135f3a364798
JamesBender <James@JamesCBender>
2013-12-19 15:33:19 -0500

    added integration test project, nunit and ninject

---
+++ WebDirectory.IntegrationTests/Web.Core/Controllers/PersonControllerTests.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace WebDirectory.IntegrationTests.Web.Core.Controllers
+{
+    [TestFixture]
+    public class WhenUsingThePersonController
+    {
+        
+    }
+}
